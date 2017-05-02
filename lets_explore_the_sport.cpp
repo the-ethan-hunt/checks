e8f3456aff82229b670c593577e61539b45935f8
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2017-05-02 07:40:49 +0530

    Create lets_explore_the_sport.cpp
    A program describing the use of comma operator and Boolean operator in an innovative way.

---
+++ lets_explore_the_sport.cpp
@@ -0,0 +1,13 @@
+#define heart "iostream"
+#include heart
+
+using namespace std;
+
+int main()
+{
+	int fun = 0, knowledge = 0;
+	bool practice = 0;
+	while(practice && fun >= 0)	 //with practice
+		++fun, ++knowledge; 	//comes fun and knowledge
+	return 0;
+}
